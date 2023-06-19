0bbb3c6dc91bae686b1f264612842896a99cb014
Matt Taggart <mtaggart89@gmail.com>
2023-06-18 23:15:09 -0700

    udpate app name to projectpilot

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,11 @@ export default function routes(app, addon) {
 
   // This is an example route used by "generalPages" module (see atlassian-connect.json).
   // Verify that the incoming request is authenticated with Atlassian Connect.
-  app.get("/hello-world", addon.authenticate(), (req, res) => {
+  app.get("/project-pilot", addon.authenticate(), (req, res) => {
     // Rendering a template is easy; the render method takes two params: the name of the component or template file, and its props.
     // Handlebars and jsx are both supported, but please note that jsx changes require `npm run watch-jsx` in order to be picked up by the server.
     res.render(
-      "hello-world.jsx", // change this to 'hello-world.jsx' to use the Atlaskit & React version
+      "project-pilot.jsx", // change this to 'hello-world.jsx' to use the Atlaskit & React version
       {
         title: "Atlassian Connect",
         //, issueId: req.query['issueId']
